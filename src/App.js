1d19ede40c0da134c398d64430c88f09c8d53bc9
Trista Cheng <trista8130@gmail.com>
2021-05-14 17:57:31 -0400

    add handle click

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React, { useEffect, useState } from "react";
 import cardList from "./cardList";
 import { randomArr } from "./fn";
+import bg from "./cards/blue_back.png";
 
 function App() {
   const [playList, setPlayList] = useState(null);
-  console.log(randomArr(6, cardList));
+  const [showList, setShowList] = useState([]);
+  const [pairList, setPairList] = useState([]);
 
   useEffect(() => {
-    const res = randomArr(6, cardList);
+    const res = randomArr(4, cardList); //set amount
     setPlayList(res);
+    const list = new Array(4 * 2).fill(false);
+    setShowList(list);
   }, []);
-  console.log(playList);
+  useEffect(() => {
+    if (pairList && pairList.length === 2) {
+      console.log(pairList[1].num);
+      let firstCard = pairList[0];
+      let secondCard = pairList[1];
+      if (firstCard.num !== secondCard.num) {
+        setPairList([]);
+        console.log(showList);
+        showList[firstCard.i] = false;
+        // setTimeout({showList[secondCard.i] = false},200)
+       showList[secondCard.i] = false;
+        setShowList([...showList]);
+      }
+    
+      //console.log(pairList);
+    }
+  }, [pairList]);
+  console.log(showList);
+  const handleClick = (i, num) => {
+    console.log(i);
+    showList[i] = true;
+    setShowList([...showList]);
+
+    if (pairList.length <= 1) {
+      pairList.push({ i, num });
+      setPairList([...pairList]);
+    }
+    //console.log(showList);
+    //console.log(pairList);
+  };
+
   return (
-    <div className="App">
-      123
-      {playList &&
-        playList.map((card, i) => (
-          <div key={i}>
-            <div>{card.num}</div>
-            <div>{card.color}</div>
-          </div>
-        ))}
+    <div>
+      <div className="header">
+        <h1>Flip cards game</h1>
+        <br></br>
+        <h3>Please enter cards amount you would like to play!</h3>
+        <input></input>
+        <p>You can only choose amount from 1 to 13!</p>
+      </div>
+      <div className="cards">
+        {playList &&
+          playList.map((card, i) => {
+            return (
+              <div
+                key={i}
+                className="cards__box"
+                onClick={() => handleClick(i, card.num)}
+              >
+                {showList[i] ? (
+                  <img src={card.img} alt="card"></img>
+                ) : (
+                  <img src={bg} alt="bg"></img>
+                )}
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
--- src/cardList.js
+++ src/cardList.js
@@ -1,82 +1,146 @@
+import AH from "./cards/AH.png";
+import AS from "./cards/AS.png";
+import AD from "./cards/AD.png";
+import AC from "./cards/AC.png";
+
+import C2H from "./cards/2H.png";
+import C2S from "./cards/2S.png";
+import C2D from "./cards/2D.png";
+import C2C from "./cards/2C.png";
+
+import C3H from "./cards/3H.png";
+import C3S from "./cards/3S.png";
+import C3D from "./cards/3D.png";
+import C3C from "./cards/3C.png";
+
+import C4H from "./cards/4H.png";
+import C4S from "./cards/4S.png";
+import C4D from "./cards/4D.png";
+import C4C from "./cards/4C.png";
+
+import C5H from "./cards/5H.png";
+import C5S from "./cards/5S.png";
+import C5D from "./cards/5D.png";
+import C5C from "./cards/5C.png";
+
+import C6H from "./cards/6H.png";
+import C6S from "./cards/6S.png";
+import C6D from "./cards/6D.png";
+import C6C from "./cards/6C.png";
+
+import C7H from "./cards/7H.png";
+import C7S from "./cards/7S.png";
+import C7D from "./cards/7D.png";
+import C7C from "./cards/7C.png";
+
+import C8H from "./cards/8H.png";
+import C8S from "./cards/8S.png";
+import C8D from "./cards/8D.png";
+import C8C from "./cards/8C.png";
+
+import C9H from "./cards/9H.png";
+import C9S from "./cards/9S.png";
+import C9D from "./cards/9D.png";
+import C9C from "./cards/9C.png";
+
+import C10H from "./cards/10H.png";
+import C10S from "./cards/10S.png";
+import C10D from "./cards/10D.png";
+import C10C from "./cards/10C.png";
+
+import CJH from "./cards/JH.png";
+import CJS from "./cards/JS.png";
+import CJD from "./cards/JD.png";
+import CJC from "./cards/JC.png";
+
+import CQH from "./cards/QH.png";
+import CQS from "./cards/QS.png";
+import CQD from "./cards/QD.png";
+import CQC from "./cards/QC.png";
+
+import CKH from "./cards/KH.png";
+import CKS from "./cards/KS.png";
+import CKD from "./cards/KD.png";
+import CKC from "./cards/KC.png";
+
 const cardList = [
   [
-    { num: 1, img: require("./cards/AH.png"), color: "hearts" },
-    { num: 1, img: require("./cards/AS.png"), color: "spades" },
-    { num: 1, img: require("./cards/AD.png"), color: "diamonds" },
-    { num: 1, img: require("./cards/AC.png"), color: "clubs" },
+    { num: 1, img: AH, color: "hearts" },
+    { num: 1, img: AS, color: "spades" },
+    { num: 1, img: AD, color: "diamonds" },
+    { num: 1, img: AC, color: "clubs" },
   ],
   [
-    { num: 2, img: require("./cards/2H.png"), color: "hearts" },
-    { num: 2, img: require("./cards/2S.png"), color: "spades" },
-    { num: 2, img: require("./cards/2D.png"), color: "diamonds" },
-    { num: 2, img: require("./cards/2C.png"), color: "clubs" },
+    { num: 2, img: C2H, color: "hearts" },
+    { num: 2, img: C2S, color: "spades" },
+    { num: 2, img: C2D, color: "diamonds" },
+    { num: 2, img: C2C, color: "clubs" },
   ],
   [
-    { num: 3, img: require("./cards/3H.png"), color: "hearts" },
-    { num: 3, img: require("./cards/3S.png"), color: "spades" },
-    { num: 3, img: require("./cards/3D.png"), color: "diamonds" },
-    { num: 3, img: require("./cards/3C.png"), color: "clubs" },
+    { num: 3, img: C3H, color: "hearts" },
+    { num: 3, img: C3S, color: "spades" },
+    { num: 3, img: C3D, color: "diamonds" },
+    { num: 3, img: C3C, color: "clubs" },
   ],
   [
-    { num: 4, img: require("./cards/4H.png"), color: "hearts" },
-    { num: 4, img: require("./cards/4S.png"), color: "spades" },
-    { num: 4, img: require("./cards/4D.png"), color: "diamonds" },
-    { num: 4, img: require("./cards/4C.png"), color: "clubs" },
+    { num: 4, img: C4H, color: "hearts" },
+    { num: 4, img: C4S, color: "spades" },
+    { num: 4, img: C4D, color: "diamonds" },
+    { num: 4, img: C4C, color: "clubs" },
   ],
   [
-    { num: 5, img: require("./cards/5H.png"), color: "hearts" },
-    { num: 5, img: require("./cards/5S.png"), color: "spades" },
-    { num: 5, img: require("./cards/5D.png"), color: "diamonds" },
-    { num: 5, img: require("./cards/5C.png"), color: "clubs" },
+    { num: 5, img: C5H, color: "hearts" },
+    { num: 5, img: C5S, color: "spades" },
+    { num: 5, img: C5D, color: "diamonds" },
+    { num: 5, img: C5C, color: "clubs" },
   ],
   [
-    { num: 6, img: require("./cards/6H.png"), color: "hearts" },
-    { num: 6, img: require("./cards/6S.png"), color: "spades" },
-    { num: 6, img: require("./cards/6D.png"), color: "diamonds" },
-    { num: 6, img: require("./cards/6C.png"), color: "clubs" },
+    { num: 6, img: C6H, color: "hearts" },
+    { num: 6, img: C6S, color: "spades" },
+    { num: 6, img: C6D, color: "diamonds" },
+    { num: 6, img: C6C, color: "clubs" },
   ],
   [
-    { num: 7, img: require("./cards/7H.png"), color: "hearts" },
-    { num: 7, img: require("./cards/7S.png"), color: "spades" },
-    { num: 7, img: require("./cards/7D.png"), color: "diamonds" },
-    { num: 7, img: require("./cards/7C.png"), color: "clubs" },
+    { num: 7, img: C7H, color: "hearts" },
+    { num: 7, img: C7S, color: "spades" },
+    { num: 7, img: C7D, color: "diamonds" },
+    { num: 7, img: C7C, color: "clubs" },
   ],
   [
-    { num: 8, img: require("./cards/8H.png"), color: "hearts" },
-    { num: 8, img: require("./cards/8S.png"), color: "spades" },
-    { num: 8, img: require("./cards/8D.png"), color: "diamonds" },
-    { num: 8, img: require("./cards/8C.png"), color: "clubs" },
+    { num: 8, img: C8H, color: "hearts" },
+    { num: 8, img: C8S, color: "spades" },
+    { num: 8, img: C8D, color: "diamonds" },
+    { num: 8, img: C8C, color: "clubs" },
   ],
   [
-    { num: 9, img: require("./cards/9H.png"), color: "hearts" },
-    { num: 9, img: require("./cards/9S.png"), color: "spades" },
-    { num: 9, img: require("./cards/9D.png"), color: "diamonds" },
-    { num: 9, img: require("./cards/9C.png"), color: "clubs" },
+    { num: 9, img: C9H, color: "hearts" },
+    { num: 9, img: C9S, color: "spades" },
+    { num: 9, img: C9D, color: "diamonds" },
+    { num: 9, img: C9C, color: "clubs" },
   ],
   [
-    { num: 10, img: require("./cards/10H.png"), color: "hearts" },
-    { num: 10, img: require("./cards/10S.png"), color: "spades" },
-    { num: 10, img: require("./cards/10D.png"), color: "diamonds" },
-    { num: 10, img: require("./cards/10C.png"), color: "clubs" },
+    { num: 10, img: C10H, color: "hearts" },
+    { num: 10, img: C10S, color: "spades" },
+    { num: 10, img: C10D, color: "diamonds" },
+    { num: 10, img: C10C, color: "clubs" },
   ],
   [
-    { num: 11, img: require("./cards/JH.png"), color: "hearts" },
-    { num: 11, img: require("./cards/JS.png"), color: "spades" },
-    { num: 11, img: require("./cards/JD.png"), color: "diamonds" },
-    { num: 11, img: require("./cards/JC.png"), color: "clubs" },
+    { num: 11, img: CJH, color: "hearts" },
+    { num: 11, img: CJS, color: "spades" },
+    { num: 11, img: CJD, color: "diamonds" },
+    { num: 11, img: CJC, color: "clubs" },
   ],
   [
-    { num: 12, img: require("./cards/QH.png"), color: "hearts" },
-    { num: 12, img: require("./cards/QS.png"), color: "spades" },
-    { num: 12, img: require("./cards/QD.png"), color: "diamonds" },
-    { num: 12, img: require("./cards/QC.png"), color: "clubs" },
+    { num: 12, img: CQH, color: "hearts" },
+    { num: 12, img: CQS, color: "spades" },
+    { num: 12, img: CQD, color: "diamonds" },
+    { num: 12, img: CQC, color: "clubs" },
   ],
   [
-    { num: 13, img: require("./cards/KH.png"), color: "hearts" },
-    { num: 13, img: require("./cards/KS.png"), color: "spades" },
-    { num: 13, img: require("./cards/KD.png"), color: "diamonds" },
-    { num: 13, img: require("./cards/KC.png"), color: "clubs" },
+    { num: 13, img: CKH, color: "hearts" },
+    { num: 13, img: CKS, color: "spades" },
+    { num: 13, img: CKD, color: "diamonds" },
+    { num: 13, img: CKC, color: "clubs" },
   ],
 ];
-
 export default cardList;
